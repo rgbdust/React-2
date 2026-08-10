8b40fea825868f65ac4457d082c6089d5fc73077
Radhesh <rgbdust@gmail.com>
2026-08-10 10:45:37 +0530

    feat: update App component content and adjust corresponding unit test expectations

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@
 import './App.css';
 
 function App() {
-  const jsx = <h1>YO bro</h1>
-  return jsx
+  const test = ["React", "js", "software"]
+  return (
 
+    <div className="App">
+      <h1>You know what</h1>
+      <p>I love {test[0]}</p>
+    </div>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders yo supp text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/Yo Supp!!/i);
+  expect(textElement).toBeInTheDocument();
 });
