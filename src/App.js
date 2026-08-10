2a0a2d8c07bab537373976e263fa35863feff8c8
Radhesh <rgbdust@gmail.com>
2026-08-10 16:03:01 +0530

    feat: update test array formatting, display array, and add conditional rendering for boolean and ternary values in App component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import './App.css';
 
 function App() {
-  const test = ["React", "js", "software"]
+  const test = ["React", " js", " software"]
   return (
 
     <div className="App">
       <h1>You know what</h1>
-      <p>I love {test[0]}</p>
+      <p>I love {test}</p>
+      <h1>{String(true)}</h1>
+
+      {2 > 3 ? <h6>Such too much</h6> : <h6>kabhi kabhi chup rehna hee behtr hai</h6>}
     </div>
   );
 }
