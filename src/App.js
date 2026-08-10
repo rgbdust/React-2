10245728e1661a358154102d29bb5185309d3292
Radhesh <rgbdust@gmail.com>
2026-08-10 17:39:42 +0530

    feat: render list items from test array using map in App component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
       <h1>You know what</h1>
       <p>I love {test}</p>
       <h1>{String(true)}</h1>
+      {
+        test.map(value => <h1>{value}</h1>)
+        // Map is used instead of loops in JSX. 
+      }
 
       {2 > 3 ? <h6>Such too much</h6> : <h6>kabhi kabhi chup rehna hee behtr hai</h6>}
     </div>
